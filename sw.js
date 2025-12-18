41a30ed00a5e5c236d1a953659a396f82a22c605
VortexRay <themasterofspirits@gmail.com>
2025-12-18 10:28:24 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
+// Flash Network service-worker
+const CACHE = 'flash-network-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        './',               // root
+        './index.html',     // main page
+        './manifest.json'   // PWA manifest
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedRes => {
+      // return cached version first, fall back to network
+      return cachedRes || fetch(e.request);
+    })
+  );
+});
 
